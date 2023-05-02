619e487bacb50c04491a7aea47559c5fbe1ab32a
ghost <blackghost2693@gmail.com>
2023-05-02 23:10:56 +0330

    add static files routes

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('room/', include("rooms.urls")),
 ]
+
+# static file routes
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
