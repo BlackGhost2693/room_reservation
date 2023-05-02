801faaf4a6d790840a5ae53d1d95a97bcfa8b3bb
ghost <blackghost2693@gmail.com>
2023-05-02 21:22:21 +0330

    add room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    """
+    model class for rooms
+    """
+    ROOM_TYPE = (
+        ("Single", "Single"),
+        ("Double", "Double"),
+        ("Triple", "Triple"),
+        ("Quad", "Quad"),
+        ("VIP", "VIP")
+    )
+    type = models.CharField(choices=ROOM_TYPE, max_length=6)
+    price = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+
+
+class Reservation(models.Model):
+    """
+    model class for users reservations
+    """
+    check_in = models.DateField()
+    check_out = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    reservationist = models.CharField(max_length=50)
+    phone = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f"{self.room}|{self.reservationist}"
+
+
